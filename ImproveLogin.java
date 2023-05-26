f3a11f3b4f3591e7c4bd3d1a515cd43a90cfb351
Klien Nazareno <kliennazareno1@gmail.com>
2023-05-26 14:48:53 +0800

    ayaw ko na

--- ImproveLogin.java
+++ ImproveLogin.java
@@ -1,31 +1,38 @@
 import java.awt.*;
 import java.io.FileNotFoundException;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+
+import net.miginfocom.swing.MigLayout;
 
 public class ImproveLogin extends JFrame {
     public void loginFrame() throws InterruptedException, FileNotFoundException {
         // init Frame
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(688, 370);
+        window.setSize(688, 400);
+        window.setLayout(new MigLayout("fill"));
+        
+        JButton login = new JButton("Login");
 
-    // !! change to JLabel!!
-    JLabel painting = new JLabel() {
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            Image img = Toolkit.getDefaultToolkit().getImage("src/icons/Intro.png");
-            // Image img = Toolkit.getDefaultToolkit().getImage("src/loginbackground.png");
+        // !! change to JLabel!!
+        JLabel painting = new JLabel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                ImageIcon img4 = new ImageIcon("src/icons/lgbg.png");
+                Image img = img4.getImage();
 
-            g.drawImage(img, 0, 0, this);
-            repaint();
-        }
-    };
-    window.add(painting);
-    window.setVisible(true);
+                g.drawImage(img, 0, 0, this);
+                repaint();
+            }
+        };
+        window.add(painting, "center");
+        window.add(login, "cell 0 0");
+        window.setVisible(true);
     }
 
     public void loginPage() throws FileNotFoundException, InterruptedException{
